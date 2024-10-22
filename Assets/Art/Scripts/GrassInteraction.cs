adb0ce6f9e61f6f63c7ea46a4322b2942c6de311
Alakyr <manugandara0@gmail.com>
2024-10-22 00:47:33 -0300

    a la grande le puse cuca

--- Assets/Art/Scripts/GrassInteraction.cs
+++ Assets/Art/Scripts/GrassInteraction.cs
@@ -7,31 +7,30 @@ public class GrassInteraction : MonoBehaviour
     [Header("<color=green>Rendering</color>")]
     [SerializeField] private string _sqrDistName = "_SqrDistance";
 
-
     private float _sqrDist = 0.0f;
 
     private Renderer[] _renderers;
-    private Material[] _materials;
+    private Material[] _mats;
 
-    void Start()
+    private void Start()
     {
         _renderers = GetComponentsInChildren<Renderer>();
 
-        _materials = new Material[_renderers.Length];
+        _mats = new Material[_renderers.Length];
 
-        for (int i = 0; i < _renderers.Length; i++) 
+        for (int i = 0; i < _renderers.Length; i++)
         {
-            _materials[i] = _renderers[i].material;
+            _mats[i] = _renderers[i].material;
         }
     }
 
-    void Update()
+    private void Update()
     {
-        _sqrDist = Vector3.SqrMagnitude(transform.position - GameManager.Instance.transform.position);
+        _sqrDist = Vector3.SqrMagnitude(transform.position - GameManager.Instance.Player.transform.position);
 
-        for (int i = 0; i < _materials.Length; i++) 
+        for (int i = 0; i < _mats.Length; i++)
         {
-            _materials[i].SetFloat(_sqrDistName, _sqrDist);
+            _mats[i].SetFloat(_sqrDistName, _sqrDist);
         }
     }
 }
