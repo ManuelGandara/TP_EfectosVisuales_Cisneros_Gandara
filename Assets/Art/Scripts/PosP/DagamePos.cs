7a9fec0c2d89c4e2f0a1ad8ea0db4dff9c8b5250
Alakyr <manugandara0@gmail.com>
2024-12-04 17:49:25 -0300

    postprocesado terminado

--- Assets/Art/Scripts/PosP/DagamePos.cs
+++ Assets/Art/Scripts/PosP/DagamePos.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class DamagePos : MonoBehaviour
 {
     [Header("<color=green>Rendering</color>")]
-    [SerializeField] private string _sqrDistName = "_DistDist";
+    [SerializeField] private string _sqrDistName2 = "_DistDist2";
 
-    private float _DistDist = 0.0f;
+    private float _DistDist2 = 0.0f;
 
     private Renderer[] _renderers;
     [SerializeField] Material _damage;
@@ -20,16 +20,16 @@ public class DamagePos : MonoBehaviour
 
     private void Update()
     {
-        //_DistDist = Vector3.SqrMagnitude(_body.transform.position - GameManager.Instance.player.transform.position);
+        _DistDist2 = Vector3.SqrMagnitude(_body.transform.position - GameManager.Instance.player.transform.position);
 
-       /* if (_DistDist <= 100 && GameManager.Instance.onFire == true)
+        if (_DistDist2 <= 100 && GameManager.Instance.onFire == true)
         {
-            _damage.SetFloat(_sqrDistName, 0);
+            _damage.SetFloat(_sqrDistName2, 0);
         }
         else
         {
-            _damage.SetFloat(_sqrDistName, 1);
-        }*/
+            _damage.SetFloat(_sqrDistName2, 1);
+        }
         if (Input.GetKeyDown(KeyCode.R))
         {
             FullScreenManager.Instance.M_Damage.SetFloat(FullScreenManager.Instance.DistanceName, 0);
